540955b9261692cfe9299820ef65181972c53a8e
0aleksandar0 <92309291+0aleksandar0@users.noreply.github.com>
2021-11-01 01:37:58 +0100

    post and get are added
    post and get are added with status response

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+import Cors from "cors";
 
 // App Config
 const app = express(); //create a instance
@@ -8,18 +10,42 @@ const connection_url =
   "mongodb+srv://admin:KUCG4V1npsT4e63l@cluster0.zxzvt.mongodb.net/whoamidb?retryWrites=true&w=majority";
 
 // Middlewares
+app.use(express.json());
+app.use(Cors());
 
 // DB config
 mongoose.connect(connection_url, {
   // these parameters are used for make the connection smooth because mongoose is under constant development and evolutionn
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useUndifiedTopology: true,
+  /* useCreateIndex: true, */
+  /* useUndifiedTopology: true, */
 });
 
 // API Endpoints
-
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
+
+app.post("/whoami/cards", (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    // a callback function with error and data parameters
+    if (err) {
+      res.status(500).send(err); // 500 means internal server error
+    } else {
+      res.status(201).send(data); // create was successful and the data can be send
+    }
+  });
+});
+
+app.get("/whoami/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
